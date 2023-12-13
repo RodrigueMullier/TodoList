fa7aab94bb4c09688dd5c9253b86cdf2edff646e
Rodrigue <rodrigue.mullier.pro@gmail.com>
2023-12-13 16:56:21 +0100

    add packages / folders creation

---
+++ TodoList/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using TodoList.Utils.Enums;
+
+namespace TodoList.Models
+{
+    public class Item
+    {
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required ItemStatus Status { get; set; } = ItemStatus.Default;
+    }
+}
